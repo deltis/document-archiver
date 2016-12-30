dc7047183030db5c1a7af4193a25212f15cf2c31
Benoit Devos <benoit.devos@deltis.be>
2016-12-30 18:08:59 +0100

    Indentation set to 4 spaces

--- src/main/java/be/deltis/documentarchiver/model/DocumentModel.java
+++ src/main/java/be/deltis/documentarchiver/model/DocumentModel.java
@@ -7,56 +7,56 @@ import javax.xml.bind.annotation.XmlType;
 @XmlType(propOrder={"supplier", "name", "documentType", "originalFormat", "fileNameRegExp"})
 public class DocumentModel {
 
-	private String name ;
-	private DocumentType documentType ;
-	private String supplier ;
-	private OriginalFormat originalFormat ;
-	private String fileNameRegExp ;
+    private String name ;
+    private DocumentType documentType ;
+    private String supplier ;
+    private OriginalFormat originalFormat ;
+    private String fileNameRegExp ;
 
     public DocumentModel() {
     }
 
     /**
-	 * Copy constructor.
-	 * @param other the DocumentModel to copy values from
-	 */
-	DocumentModel(DocumentModel other) {
+     * Copy constructor.
+     * @param other the DocumentModel to copy values from
+     */
+    DocumentModel(DocumentModel other) {
         this.name = other.name ;
         this.documentType = other.documentType ;
         this.supplier = other.supplier ;
         this.originalFormat = other.originalFormat ;
         this.fileNameRegExp = other.fileNameRegExp ;
-	}
+    }
 
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public DocumentType getDocumentType() {
-		return documentType;
-	}
-	public void setDocumentType(DocumentType documentType) {
-		this.documentType = documentType;
-	}
-	public String getSupplier() {
-		return supplier;
-	}
-	public void setSupplier(String supplier) {
-		this.supplier = supplier;
-	}
-	public OriginalFormat getOriginalFormat() {
-		return originalFormat;
-	}
-	public void setOriginalFormat(OriginalFormat originalFormat) {
-		this.originalFormat = originalFormat;
-	}
-	public String getFileNameRegExp() {
-		return fileNameRegExp;
-	}
-	public void setFileNameRegExp(String fileNameRegExp) {
-		this.fileNameRegExp = fileNameRegExp;
-	}	
-	
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public DocumentType getDocumentType() {
+        return documentType;
+    }
+    public void setDocumentType(DocumentType documentType) {
+        this.documentType = documentType;
+    }
+    public String getSupplier() {
+        return supplier;
+    }
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+    public OriginalFormat getOriginalFormat() {
+        return originalFormat;
+    }
+    public void setOriginalFormat(OriginalFormat originalFormat) {
+        this.originalFormat = originalFormat;
+    }
+    public String getFileNameRegExp() {
+        return fileNameRegExp;
+    }
+    public void setFileNameRegExp(String fileNameRegExp) {
+        this.fileNameRegExp = fileNameRegExp;
+    }
+
 }
